5302040e251abe5708681330150a51ebdaffeeb2
Kelly Zegelaar <knzegela@avans.nl>
2018-02-02 16:30:27 +0100

    Added some components to the UI

--- library/presentation/StatistixUI.java
+++ library/presentation/StatistixUI.java
@@ -23,8 +23,39 @@ public class StatistixUI implements Runnable {
         frame.setVisible(true);
     }
 
+    //here are all the components created that are visible in the application
     private void createComponents(Container container){
+        container.setLayout(new BorderLayout());
 
+        container.add(createChooseAccount(), BorderLayout.NORTH);
+        container.add(new JPanel(), BorderLayout.CENTER);
+        container.add(createBottomApp(), BorderLayout.SOUTH);
     }
+
+    private JTabbedPane createChooseAccount(){
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        tabbedPane.addTab("Kelly", createOverview());
+
+        return tabbedPane;
+    }
+
+    private JPanel createOverview(){
+        JPanel panel = new JPanel();
+
+        return panel;
+    }
+
+    //this creates the textline at the end of the app
+    private JPanel createBottomApp(){
+        JPanel bottomApp = new JPanel(new GridLayout(1, 2));
+
+        bottomApp.add(new JLabel("Netflix Statistix"));
+        bottomApp.add(new JLabel("Informatica 2018 - Kelly Zegelaar 2131095"));
+
+        return bottomApp;
+    }
+
+
 }
 
