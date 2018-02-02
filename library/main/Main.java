d10221a9c5ba2ce325fa4ad3a9373228f491a8ca
Kelly Zegelaar <knzegela@avans.nl>
2018-02-02 15:37:30 +0100

    added a basic UI

--- library/main/Main.java
+++ library/main/Main.java
@@ -1,4 +1,17 @@
 package library.main;
 
+import library.presentation.StatistixUI;
+
+import javax.swing.SwingUtilities;
+
 public class Main {
+    public static void main(String[] args){
+        StatistixUI ui = new StatistixUI();
+
+        //wat hieronder staat kan als het goed is later weg gewerkt worden\
+        //de vraag is alleen hoe..
+        //Als dit is gebeurd dan moet ook de import javax.swing.SwingUtilities weggehaald worden.
+        SwingUtilities.invokeLater(ui);
+    }
+
 }
--- library/presentation/StatistixUI.java
+++ library/presentation/StatistixUI.java
@@ -1,4 +1,30 @@
+/*  This class creates the visible UI using the Runnable interface.
+    A javax.swing.* import is needed for the use of buttons ect.
+*/
+
 package library.presentation;
 
-public class StatistixUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class StatistixUI implements Runnable {
+    private JFrame frame;
+
+    @Override
+    public void run() {
+        frame = new JFrame("Netflix Statistix");
+        frame.setPreferredSize(new Dimension(1200, 750));
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        createComponents(frame.getContentPane());
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void createComponents(Container container){
+
+    }
 }
+
