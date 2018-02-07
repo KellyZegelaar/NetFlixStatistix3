43f10231daf884182f8b49828bc1d9f7f9988dfb
Kelly Zegelaar <knzegela@avans.nl>
2018-02-07 18:56:45 +0100

    added movieManager

---
+++ library/applicationLogic/MovieManager.java
@@ -0,0 +1,20 @@
+package library.applicationLogic;
+
+import library.dataAccess.Factory;
+import library.domain.Movie;
+
+import java.util.ArrayList;
+
+public class MovieManager {
+    private Factory factory;
+    private ArrayList<Movie> movies;
+
+    public MovieManager(Factory factory) {
+        this.factory = factory;
+        readMovies();
+    }
+
+    public void readMovies(){
+        movies = factory.createMovieDao().readMovies();
+    }
+}
